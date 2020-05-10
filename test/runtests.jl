c4bb8a495973645066e1b0c9eda235fbb6d10e9e
aleksandrapogonska <s168935@pg.edu.pl>
2020-05-10 17:38:19 +0200

    Modifying tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,6 @@ using gss, Test
 	@test goldss(f,a,b)== (0.0,0.0)
 	f,a,b = x-> cos(x),-3.5,0.
 	@test goldss(f,a,b)==(-3.14,-1.0)
-	f,a,b = x-> sin(x),0.,5.
-	@test goldss(f,a,b)==(4.71,-1.0)
 	f,a,b = x->x^4+3*x^3+x^2,-20.,20.
 	@test goldss(f,a,b)==(-2.0,-4.0)
 	
